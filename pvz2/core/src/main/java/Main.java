04f8d3d11982a058c33203a2a8e4f848dd6c7164
Amirmohammad.F <amirmohammadf1234567@gmail.com>
2026-08-12 20:20:55 +0330

    moved game view to libgdx

--- pvz2/core/src/main/java/Main.java
+++ pvz2/core/src/main/java/Main.java
@@ -1,13 +1,17 @@
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import controllers.datacontroller.*;
 
 import models.*;
 import pvz.libpvz.pam.PamPlayer;
+import pvz.libpvz.textures.TextureBank;
 import pvz.skin.PvzSkin;
 
 
 public class Main{
-
+    private static String COMMAND = "java \"-Dpvz.assets=.\\Assets\" -jar browser.jar";
     public static void main(String[] args) {
+
         Data.deserializeUser();
         Data.setUp();
         Data.loadPlantsFromJson();
@@ -15,7 +19,7 @@ public class Main{
         MiniGameLevelManager.loadLevelsFromFile("minigames.json");
         MiniGameLevelManager.loadLevelsFromFile("minigames.json");
         while (App.getScreen() != null) {
-            App.getScreen().input();
+
         }
     }
 }
